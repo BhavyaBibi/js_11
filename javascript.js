61296094c8adaaf2cf8e2bafd253446f8bfeff04
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-04-26 07:00:38 +0400

    function added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("hi");
+function myFunc() {
+    let a = 5;
+    myFuncTwo();
+    return a;
+
+}
+function myFuncTwo() {
+    console.log("completed");
+
+}
+function myFuncThree() {
+    let b = 10;
+    myFuncTwo();
+    return b;
+}
+console.log(myFunc());
+console.log(myFuncThree());
