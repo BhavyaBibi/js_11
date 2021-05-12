608651d9df7bea364f402cd979ee0e306f971197
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-05-12 13:37:53 +0400

    switch condition added

--- javascript.js
+++ javascript.js
@@ -12,13 +12,31 @@
 
 // if condition
 
-let johnAge = 19;
-if (johnAge === 45) {
-    console.log("you are old")
+// let johnAge = 21;
+// if (johnAge === 45) {
+//     console.log("you are old")
+// }
+// else if (johnAge <= 30 && johnAge >= 20) {
+//     console.log("you are about to be old")
+// }
+// else {
+//     console.log("you are young")
+// }
+
+//switch condition
+let fruits = "banana";
+
+switch (fruits) {
+    case "orange":
+        console.log("i bought fruit orange");
+        break;
+    case "banana":
+        console.log("i bought fruit banana");
+        break;
+    default:
+        console.log("no fruits found")
+
+
+
 }
-else if (johnAge <= 30 && johnAge >= 20) {
-    console.log("you are about to be old")
-}
-else {
-    console.log("you are young")
-}
+
