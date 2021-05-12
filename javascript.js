537ed773034b4bb70259d90828681d8fec037a59
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-05-12 13:29:18 +0400

    if condition used

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
 // objects
 
-let myobj = {
+// let myobj = {
 
-    firstname: "john",
-    lastname: "doe"
-};
+//     firstname: "john",
+//     lastname: "doe"
+// };
 
-myobj.firstname = "jacob";
-delete myobj.firstname;
-console.log(myobj);
+// myobj.firstname = "jacob";
+// delete myobj.firstname;
+// console.log(myobj);
+
+// if condition
+
+let johnAge = 19;
+if (johnAge === 45) {
+    console.log("you are old")
+}
+else if (johnAge <= 30 && johnAge >= 20) {
+    console.log("you are about to be old")
+}
+else {
+    console.log("you are young")
+}
