8c0de086f6b4735707c2a6bb05b313a16fda36ef
BhavyaBibi <bhavyabibi007@gmail.com>
2021-07-27 12:13:27 +0400

    the changes added

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // spread operator
 
-const globalPlayer = ["bhavya", "bibi", "krish"];
-const globalPlayer2 = ["bhavu", "bibu", "krisu"];
+// const globalPlayer = ["bhavya", "bibi", "krish"];
+// const globalPlayer2 = ["bhavu", "bibu", "krisu"];
 
 
-const player1 = globalPlayer.concat(globalPlayer2)
+// const player1 = globalPlayer.concat(globalPlayer2)
 
-console.log(player1)
+// console.log(player1)
 
 // console.log(globalPlayer);
 // console.log(globalPlayer2);
 
+//rest
 
+
+const globalPlayer = ["bhavya", "bibi", "krish"];
+
+//ethil bhavyae nammuke vilikanum
+
+console.log(globalPlayer[0])
