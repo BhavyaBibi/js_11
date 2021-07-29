0fb772bd69c2047e6332cbe10c1c6bc41ba85061
BhavyaBibi <bhavyabibi007@gmail.com>
2021-07-29 17:44:07 +0400

    localstorage added

---
+++ localstorage.js
@@ -0,0 +1,8 @@
+// localstorage.setItem("id")
+// localstorage.getItem()
+// localstorage.removeItem()
+// localstorage.clear()
+
+
+localStorage.setItem("id","0")
+document.write(localStorage.getItem("id"))
