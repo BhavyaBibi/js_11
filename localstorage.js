8e8d3d0b3ea451b556daeea216f4de0e7ee07bdd
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-01 12:54:39 +0400

    local storage added

--- localstorage.js
+++ localstorage.js
@@ -2,11 +2,12 @@
 const input=document.querySelector("input")
 const loginBtn =document.querySelector("#login")
 const logoutBtn=document.querySelector("#logout")
-const h1 = document.querySelector("h1")
+const h1 = document.querySelector("h1");
 
-    loginBtn.onclick =()=>{
+
+    loginBtn.onclick=()=>{
         if(input.value){
-            localStorage.setitem("token",input.value)
+            localStorage.setItem("token",input.value)
             h1.innerText="Welcome user"
         }
 
