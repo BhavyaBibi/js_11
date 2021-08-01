8eed5b53165922c28207012605870f27471e61a3
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-01 13:01:57 +0400

    locaal storage datas also added

--- localstorage.js
+++ localstorage.js
@@ -5,16 +5,16 @@ const logoutBtn=document.querySelector("#logout")
 const h1 = document.querySelector("h1");
 
 
-    loginBtn.onclick=()=>{
-        if(input.value){
-            localStorage.setItem("token",input.value)
-            h1.innerText="Welcome user"
+loginBtn.onclick =()=>{
+    if(input.value){
+        localStorage.setItem("token",input.value)
+        h1.innerText="Welcome user";
         }
 
     };
-    logoutBtn.onclick =() =>{
-        localStorage.clear();
-        location.reload();
-    }
+    // logoutBtn.onclick =() =>{
+    //     localStorage.clear();
+    //     location.reload();
+    // }
 
 
