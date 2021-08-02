a7cf6daaff8120fb9cc0d1eda57b9eac3b7631fe
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-02 09:24:14 +0400

    just add the method of integer covertion method

--- module.js
+++ module.js
@@ -1,7 +1,2 @@
-import { myString as str ,myFunc as ter} from "./variable.js";
-
-import handleInputAsString from "./variable.js";
-
-console.log(str);
-console.log(ter);
-console.log(handleInputAsString());
+const myString = "10"
+console.log(myString)
