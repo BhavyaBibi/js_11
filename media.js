5d48d2e660fc33660cd8012292cfe6115773434a
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-03 12:01:17 +0400

    screentext now showed

--- media.js
+++ media.js
@@ -2,3 +2,6 @@ const header=document.querySelector(".show")
 console.log("header")
 
 const deviceWidth =window.matchMedia("(max-width:767px)")
+
+if(deviceWidth.matches){
+    header.classList.add("hide")
