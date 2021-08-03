3ea8e023c3370767b9caa3a54c2af841c8804f87
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-03 12:47:56 +0400

    the changes aded in the section

--- media.js
+++ media.js
@@ -1,17 +1,6 @@
 const header=document.querySelector(".show")
-console.log("header")
+const banner =document.querySelector(".banner")
 
-const deviceWidth =window.matchMedia("(max-width:767px)")
-
-// if(deviceWidth.matches){
-//     header.classList.add("hide")
-
-window.addEventListener("resize",resize);
-function resize(){
-    console.log(window.innerWidth);
-    if(deviceWidth.matches){
-        header.classList.add("hide")
-}
-else{
-header.classList.remove("hide")
-}
+ banner.addEventListener("click" , () => {
+     header.classList.toggle("hide");
+ });
