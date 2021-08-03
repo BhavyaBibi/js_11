bade3a1cd3674275a6d0179b011f81d9ab4b9140
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-03 12:27:53 +0400

    console added the data

--- media.js
+++ media.js
@@ -3,5 +3,12 @@ console.log("header")
 
 const deviceWidth =window.matchMedia("(max-width:767px)")
 
-if(deviceWidth.matches){
-    header.classList.add("hide")
+// if(deviceWidth.matches){
+//     header.classList.add("hide")
+
+window.addEventListener("resize",resize);
+function resize(){
+    console.log(window.innerWidth);
+    if(deviceWidth.matches){
+        header.classList.add("hide")
+}
