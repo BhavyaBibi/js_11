1b76c81913db39694f717dbe034b8ec1c9aeefea
BhavyaBibi <bhavyabibi007@gmail.com>
2021-08-17 12:02:26 +0400

    okay the class used function fixed in opython

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ class Student:
     def __init__(self,name,roll_no):
         self.name=name
         self.roll_no=roll_no
+
+    def get_name(self):
+        print(self.name)
+student1=Student("bhavya",12)
+student2=Student("abs",34)
+
+student1.get_name()
